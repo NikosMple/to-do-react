01086786f80fa92f0200019f5dd88315f979519a
nikos <nikosmple1998@gmail.com>
2025-01-08 12:12:38 +0200

    Updates

---
+++ precache-manifest.013725bc06131d4346d303b577886877.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb23f72329e91f4d25c8f6a85d1762e9",
+    "url": "/to-do-react/index.html"
+  },
+  {
+    "revision": "66df198cdcc7abab3518",
+    "url": "/to-do-react/static/css/main.7552affe.chunk.css"
+  },
+  {
+    "revision": "a35f33c64e0190612bf4",
+    "url": "/to-do-react/static/js/2.4de3355a.chunk.js"
+  },
+  {
+    "revision": "66df198cdcc7abab3518",
+    "url": "/to-do-react/static/js/main.1ee82b67.chunk.js"
+  },
+  {
+    "revision": "277527699d23542d0c3f",
+    "url": "/to-do-react/static/js/runtime-main.b41f4800.js"
+  }
+]);
